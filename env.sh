69245a71d74073d5b2358afb573d942e3df78566
Kushal Arora <kushal18@gmail.com>
2020-06-25 15:43:19 -0400

    updated env.sh for dp machine.

--- env.sh
+++ env.sh
@@ -1,10 +1,5 @@
-module load python/3.7
-module load  nixpkgs/16.09
-module load gcc/7.3.0
-module load nodejs
-
-source ./bin/activate
-export PATH="$PATH:${HOME}/wdir/webnav/wge/:${HOME}/opt/google/chrome/"
+source activate wge
+export PATH="$PATH:"${HOME}"/webnav/wge/chrome/"
 export RL_DEMO_DIR="third_party/miniwob-demos/"
 export RL_DATA="rl_data/"
-export MINIWOB_BASE_URL='miniwob-sandbox/html/'
+export MINIWOB_BASE_URL='file://'${HOME}'/webnav/wge/miniwob-sandbox/html/'
